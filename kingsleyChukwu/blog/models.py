c72f12ca49be1f25101524c384fdb36a49ed1cae
ChukwuKingsley <79272441+ck-alt@users.noreply.github.com>
2022-06-19 12:22:13 +0100

    Django blog app

--- kingsleyChukwu/blog/models.py
+++ kingsleyChukwu/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your models here.
+user = get_user_model()
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField(max_length=200)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date=models.DateTimeField(auto_now_add=True)
+
+
+    
